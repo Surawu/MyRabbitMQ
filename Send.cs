bda8dc0bc7ffa9281abd7cc91fc2bdc404768892
wuzhaoyu <18261446150@163.com>
2020-08-17 06:04:06 +0800

    Add project files.

---
+++ Send.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Simple.Producer
+{
+    public class Send
+    {
+        private static readonly string QUEUE_NAME = "q_test_01";
+        static void Main(string[] args)
+        {
+            var connection = ConnectionUtil.GetConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(QUEUE_NAME, false, false, false, null);
+            string message = "Hello world!";
+            channel.BasicPublish("", QUEUE_NAME, null, Encoding.UTF8.GetBytes(message));
+            
+            Console.WriteLine($"send message {message}");
+            Console.ReadLine();
+            channel.Close();
+            connection.Close();
+
+        }
+    }
+}
